fa2128452d2317729993a515a34c84e41592d849
Hilary <hilary.koerner@gmail.com>
2020-12-17 08:49:01 -0600

    worked on practice

---
+++ Python Practice/Hello_Age.py
@@ -0,0 +1,18 @@
+
+#this doesnt run correctly
+print("Hello User")
+
+name=input("What's your name?")
+
+print ("Hello" + name + "!")
+
+age = int(input("What is your age? "))
+  
+if age <= 8:
+    print("You're a baby")
+else:
+    print("You're old")
+
+
+
+
---
+++ Python Practice/netflix.py
@@ -0,0 +1,4 @@
+print("Hello, Viewer")
+
+movie=input("What movie are you looking for?")
+
