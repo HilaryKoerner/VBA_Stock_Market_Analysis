fbc9150b4ff7b3480e7ddf8ddf7bea86fb344543
Hilary <hilary.koerner@gmail.com>
2020-12-18 16:20:31 -0600

    friday homework edits

--- Python Practice/netflix.py
+++ Python Practice/netflix.py
@@ -1,4 +1,24 @@
+import os
+import csv
+
+video=input("What movie are you looking for?")
+
+csvpath = os.path.join('..', 'Python Practice', 'netflix_ratings.csv')
+
 print("Hello, Viewer")
 
-movie=input("What movie are you looking for?")
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is row header)
+    csv_header = next(csvreader)
+
+
+
+
+
 
